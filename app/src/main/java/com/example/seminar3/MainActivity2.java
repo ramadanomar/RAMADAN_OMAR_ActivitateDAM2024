c988b5ef65ee65a46e631d3ac7580a82d7944756
ramadanomar <ramadan.omaarr@gmail.com>
2024-10-15 15:51:56 +0300

    feat: seminar 3, intent button

--- app/src/main/java/com/example/seminar3/MainActivity2.java
+++ app/src/main/java/com/example/seminar3/MainActivity2.java
@@ -10,20 +10,20 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-
-public class MainActivity extends AppCompatActivity {
+public class MainActivity2 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main2);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
     }
+
 
     public void deschideActivitate(View view) {
         Intent it = new Intent(this, MainActivity2.class);
